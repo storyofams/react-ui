ecb85e654ad9b34069ce1b0af66b909f1e78e7f3
Maren Osnabrug <maren@storyofams.com>
2021-02-08 18:00:03 +0100

    test: 💡  add mock for imported css

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     '^~test-utils(.*)$': '<rootDir>/src/lib/test-utils$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/components/common/Icon/__mocks__/req',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts',
   },
   moduleDirectories: [
     'node_modules',
