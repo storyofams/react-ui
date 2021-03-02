18862ed7c8322bf43b837472aa67306ed3e4f2c3
MitchelvanBever <mitchel@storyofams.com>
2021-03-02 16:09:25 +0100

    fix: 🐛  not sure what im doing

--- src/components/common/Box/index.tsx
+++ src/components/common/Box/index.tsx
@@ -1,9 +1,31 @@
+import * as React from 'react';
 import styled from 'styled-components';
+import { BaseProps, system } from '~lib/styledSystem';
 
-import { system, BaseProps } from '~lib/styledSystem';
-import { Box as PolyBox } from '../PolyBox/index';
+export type BoxOwnProps<
+  AsElementType extends React.ElementType = React.ElementType
+> = BaseProps<AsElementType>;
 
-export const Box = styled(PolyBox)<BaseProps>`
+export type BoxProps<
+  AsElementType extends React.ElementType
+> = BoxOwnProps<AsElementType> &
+  Omit<React.ComponentProps<AsElementType>, keyof BoxOwnProps>;
+
+const defaultElement = 'div';
+
+const Polymorph = styled.div`
   font-family: inherit;
   ${system}
 `;
+
+export const Box: <
+  AsElementType extends React.ElementType = typeof defaultElement
+>(
+  props: BoxProps<AsElementType>,
+) => React.ReactElement | null = React.forwardRef(
+  (props: BoxOwnProps, ref: React.Ref<Element>) => {
+    const Element = props.as || defaultElement;
+
+    return <Polymorph as={Element} ref={ref} {...props} />;
+  },
+);
