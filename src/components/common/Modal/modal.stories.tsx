834094294d8d1413a58274ec71fc5b19f17e1d8f
Bruna da Fonseca <brunadafonseca@gmail.com>
2021-03-05 10:01:40 +0100

    feat: modal (#10)
    Co-authored-by: Bruna <bruna@storyofams.com>

---
+++ src/components/common/Modal/modal.stories.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import { Box, Text } from 'rebass/styled-components';
+import { Button } from '~components/common/Button';
+import { Modal } from '.';
+
+export default {
+  component: Modal,
+  title: 'Components/Modal',
+};
+
+// Header and renderFooter() are both equally valid props
+const Header = () => (
+  <Box width="100%" pb={1} mb={2} color="pink">
+    <Text fontSize={3} fontWeight="bold">
+      My custom header
+    </Text>
+  </Box>
+);
+
+const Footer = ({ close, children }) => (
+  <>
+    <Button variant="secondary" mr={2} onClick={close}>
+      Cancel
+    </Button>
+    {children}
+  </>
+);
+
+export const Basic = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true);
+
+  const onOk = () => {
+    closeModal();
+  };
+
+  const renderFooter = () => (
+    <Footer close={closeModal}>
+      <Button variant="primary" onClick={onOk}>
+        Confirm action
+      </Button>
+    </Footer>
+  );
+
+  return (
+    <>
+      <Box p={5}>
+        <Button variant="primary" onClick={openModal}>
+          Open modal
+        </Button>
+      </Box>
+
+      <Modal
+        isOpen={isOpen}
+        close={closeModal}
+        header="Awesome Modal Title"
+        footer={renderFooter()}
+      >
+        <Text>Are you sure you want to delete your account?</Text>
+      </Modal>
+    </>
+  );
+};
+
+export const LongText = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true);
+
+  const onOk = () => {
+    alert(`I hope I'm doing something more useful in the future.`);
+    closeModal();
+  };
+
+  const renderFooter = () => (
+    <Footer close={closeModal}>
+      <Button variant="primary" onClick={onOk}>
+        I agree
+      </Button>
+    </Footer>
+  );
+
+  return (
+    <div>
+      <Box p={5}>
+        <Button variant="primary" onClick={openModal}>
+          Open modal
+        </Button>
+      </Box>
+
+      <Modal
+        isOpen={isOpen}
+        close={closeModal}
+        header={<Header />}
+        footer={renderFooter()}
+      >
+        Very long content
+        {[...Object(Array(10)).keys()].map((i) => (
+          <p key={i}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        ))}
+      </Modal>
+    </div>
+  );
+};
