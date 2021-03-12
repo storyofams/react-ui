44bdd1fdab6a68b2de8a24d3bfe5349ec554e590
Mitchel van Bever <10127707+mitchelvanbever@users.noreply.github.com>
2021-03-12 14:14:03 +0100

    fix(box): 🐛  simplified inference of box (#17)

--- src/components/common/Box/index.tsx
+++ src/components/common/Box/index.tsx
@@ -1,42 +1,19 @@
-import React, {
-  ElementRef,
-  ElementType,
-  ForwardedRef,
-  forwardRef,
-} from 'react';
-import type {
-  PolymorphicPropsWithoutRef,
-  PolymorphicForwardRefExoticComponent,
-} from 'react-polymorphic-types';
 import styled from 'styled-components';
 
-import { system } from '~lib';
+import { system, allPropNames } from '~lib';
 import { SystemProps } from '~types/system';
 
 const _defaultElement = 'div';
 
-type CustomProps = {} & SystemProps;
+// Component-specific props should be specified here
+type Props = {} & SystemProps;
 
-type Props<
-  AsElement extends ElementType = typeof _defaultElement
-> = PolymorphicPropsWithoutRef<CustomProps, AsElement>;
-
-const Polymorph = styled.div`
+export const Box = styled(_defaultElement).withConfig({
+  shouldForwardProp: (prop, defaultValidatorFn) =>
+    allPropNames.indexOf(prop) === -1 && defaultValidatorFn(prop),
+})<Props>`
   font-family: inherit;
 
   ${(props) => props.css}
   ${system}
 `;
-
-export const Box: PolymorphicForwardRefExoticComponent<
-  CustomProps,
-  typeof _defaultElement
-> = forwardRef(function Box<
-  AsElement extends ElementType = typeof _defaultElement
->(
-  props: PolymorphicPropsWithoutRef<Props, AsElement>,
-  ref: ForwardedRef<ElementRef<AsElement>>,
-) {
-  // @ts-ignore
-  return <Polymorph {...props} ref={ref} />;
-});
--- src/components/common/Tag/index.tsx
+++ src/components/common/Tag/index.tsx
@@ -4,25 +4,29 @@ import styled from 'styled-components';
 import { Box } from '~components/common/Box';
 import { SystemProps } from '~types/system';
 
-const TagComponent = styled(Box)(({ theme, checked }) => ({
-  width: 'max-content',
-  cursor: 'pointer',
-  borderWidth: 1,
-  borderStyle: 'solid',
-  lineHeight: theme.lineHeights.normal,
-  fontSize: theme.fontSizes[1.5],
-  borderRadius: theme.radii.full,
-  padding: `${theme.space[1]}px ${theme.space[1.5]}px`,
-  color: checked ? theme.colors.white : theme.colors.grey700,
-  backgroundColor: checked ? theme.colors.primary700 : theme.colors.primary100,
-  borderColor: checked ? theme.colors.primary700 : theme.colors.primary100,
-  transition:
-    'border-color 0.18s ease-in-out, background-color 0.18s ease-in-out',
+const TagComponent = styled(Box)<{ checked: boolean }>(
+  ({ theme, checked }) => ({
+    width: 'max-content',
+    cursor: 'pointer',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    lineHeight: theme.lineHeights.normal,
+    fontSize: theme.fontSizes[1.5],
+    borderRadius: theme.radii.full,
+    padding: `${theme.space[1]}px ${theme.space[1.5]}px`,
+    color: checked ? theme.colors.white : theme.colors.grey700,
+    backgroundColor: checked
+      ? theme.colors.primary700
+      : theme.colors.primary100,
+    borderColor: checked ? theme.colors.primary700 : theme.colors.primary100,
+    transition:
+      'border-color 0.18s ease-in-out, background-color 0.18s ease-in-out',
 
-  '&:hover': {
-    borderColor: theme.colors.primary700,
-  },
-}));
+    '&:hover': {
+      borderColor: theme.colors.primary700,
+    },
+  }),
+);
 
 export interface TagProps extends SystemProps {
   checked: boolean;
