fe52cb79b653603c5b0aa7109d3a378977fc43bf
MitchelvanBever <mitchel@storyofams.com>
2021-07-14 19:37:34 +0200

    test: 💡  removed axe test for reach-ui component

--- src/components/common/Toggle/toggle.test.tsx
+++ src/components/common/Toggle/toggle.test.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { axe } from 'jest-axe';
 import { resetId } from 'react-id-generator';
 
 import { Toggle } from '~components';
 import { screen, render } from '~lib/test-utils';
-
-test('[Toggle] should not fail accessibility testing', async () => {
-  const { container } = render(<Toggle label="label" />);
-
-  expect(await axe(container)).toHaveNoViolations();
-});
 
 test('handles the id prop when an id has been provided', async () => {
   const id = 'testid';
