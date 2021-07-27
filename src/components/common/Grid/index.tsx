5f3d84a477891e470d53d1a02ea8afc7b103c703
Mitchel van Bever <10127707+mitchelvanbever@users.noreply.github.com>
2021-07-27 16:53:17 +0200

    fix(as-prop): 🐛 added polymorphic 'as' prop (#41)
    * fix(stack): 🐛  added polymorphic 'as' prop
    
    CLOSES ISSUE: #27
    
    * fix(flex): 🐛  added as prop to flex component
    
    CLOSES ISSUE: #23
    
    * fix(grid): 🐛  added polymorphic as prop to grid
    
    CLOSES ISSUE: #28

--- src/components/common/Grid/index.tsx
+++ src/components/common/Grid/index.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react';
+import React, { ElementType, ReactNode } from 'react';
+import { PolymorphicPropsWithoutRef } from 'react-polymorphic-types';
 import { ResponsiveValue } from 'styled-system';
 
 import { SystemProps } from '~lib';
@@ -7,37 +8,48 @@ import { Flex } from '~components/common/Flex';
 import { css } from '~lib/css';
 import { modifyResponsiveValue } from '~lib/modifyResponsiveValue';
 
+const GridDefaultElement = 'div';
+
 export interface CustomProps extends SystemProps {
   rowSize: ResponsiveValue<number>;
   rowGap?: ResponsiveValue<number>;
   columnGap?: ResponsiveValue<number>;
   children?: ReactNode;
+  role?: string;
 }
 
-export const Grid = ({
+type GridProps<AsElement extends ElementType = typeof GridDefaultElement> =
+  PolymorphicPropsWithoutRef<CustomProps, AsElement>;
+
+export const Grid = <
+  AsElement extends ElementType = typeof GridDefaultElement,
+>({
   rowSize,
   rowGap = 0,
   columnGap = 0,
   children,
   ...props
-}: CustomProps) => (
-  <Box {...props}>
-    <Flex
-      flexWrap="wrap"
-      ml={modifyResponsiveValue(columnGap, (val) => -val)}
-      mt={modifyResponsiveValue(rowGap, (val) => -val)}
-      css={css({
-        '& > *': {
-          flex: modifyResponsiveValue(
-            rowSize,
-            (size) => `0 1 ${(1 / size) * 100}%`,
-          ),
-          pl: columnGap,
-          pt: rowGap,
-        },
-      })}
-    >
-      {children}
-    </Flex>
-  </Box>
+}: GridProps<AsElement>) => (
+  <>
+    {/* @ts-expect-error */}
+    <Box {...props}>
+      <Flex
+        flexWrap="wrap"
+        ml={modifyResponsiveValue(columnGap, (val) => -val)}
+        mt={modifyResponsiveValue(rowGap, (val) => -val)}
+        css={css({
+          '& > *': {
+            flex: modifyResponsiveValue(
+              rowSize,
+              (size) => `0 1 ${(1 / size) * 100}%`,
+            ),
+            pl: columnGap,
+            pt: rowGap,
+          },
+        })}
+      >
+        {children}
+      </Flex>
+    </Box>
+  </>
 );
