cb85ce71046165d11d5d621f976cb69c458ff930
MitchelvanBever <mitchel@storyofams.com>
2021-09-02 17:48:30 +0200

    refactor(avatar): 🔍  reduced avatar to 75px

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -4,5 +4,5 @@ export default create({
   base: 'light',
   brandTitle: 'Story of AMS | React UI',
   brandUrl: 'https://github.com/storyofams/react-ui',
-  brandImage: 'https://avatars.githubusercontent.com/u/19343504?v=2&s=150',
+  brandImage: 'https://avatars.githubusercontent.com/u/19343504?v=2&s=75',
 })
