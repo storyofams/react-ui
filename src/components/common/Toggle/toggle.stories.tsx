ecec737fb50ffb02096c7ee403cc387540d339e9
MitchelvanBever <mitchel@storyofams.com>
2021-10-22 11:17:30 +0200

    refactor(classnames): 🔍  ensure classnames are applied to the right element

--- src/components/common/Toggle/toggle.stories.tsx
+++ src/components/common/Toggle/toggle.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Toggle } from '~components';
 
+import { styled } from '~lib';
+
 export default {
   component: Toggle,
   title: 'components/Toggle',
@@ -22,4 +24,21 @@ export default {
   parameters: { controls: { hideNoControlsWarning: true } },
 };
 
+const ExtendedToggle = styled(Toggle, {
+  baseStyles: {
+    '> .slider': {
+      backgroundColor: 'grey300',
+      borderColor: 'grey300',
+    },
+    "[data-reach-custom-checkbox][data-state='checked']": {
+      '& + .slider': {
+        backgroundColor: 'grey800',
+        borderColor: 'grey800',
+      },
+    },
+  },
+});
+
 export const Basic = (args) => <Toggle {...args} />;
+
+export const Extended = (args) => <ExtendedToggle />;
